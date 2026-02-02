85256178e167179e0092aebdd47b41b8c7406fbd
EKTC <ethancao@y7mail.com>
2026-02-02 20:35:34 +1100

    Basic implementation of auth

--- app/app/_layout.tsx
+++ app/app/_layout.tsx
@@ -9,24 +9,42 @@ import "react-native-reanimated";
 import "../global.css";
 
 import { useColorScheme } from "@/hooks/use-color-scheme";
+import { useAuthContext } from "@/hooks/use-auth-context";
+import AuthProvider from "@/providers/auth-provider";
 
 export const unstable_settings = {
   anchor: "(tabs)",
 };
 
 export default function RootLayout() {
-  const colorScheme = useColorScheme();
+  
+  
+  return (
+    <AuthProvider>
+      <RootNavigator />
+    </AuthProvider>
+  );
+}
 
+function RootNavigator() {
+  const { isLoggedIn } = useAuthContext()
+  const colorScheme = useColorScheme();
   return (
     <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen
-          name="modal"
-          options={{ presentation: "modal", title: "Modal" }}
-        />
-      </Stack>
-      <StatusBar style="auto" />
-    </ThemeProvider>
-  );
+        <Stack>
+          <Stack.Protected guard={isLoggedIn}>
+            <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+            <Stack.Screen
+              name="modal"
+              options={{ presentation: "modal", title: "Modal" }}
+            />
+          </Stack.Protected>
+          <Stack.Protected guard={!isLoggedIn}>
+            <Stack.Screen name="login" options={{ headerShown: false }} />
+          </Stack.Protected>
+        
+        </Stack>
+        <StatusBar style="auto" />
+      </ThemeProvider>
+  )
 }
---
+++ app/app/login.tsx
@@ -0,0 +1,95 @@
+import { Stack, router } from 'expo-router'
+import { StyleSheet, TextInput, Button, Alert } from 'react-native'
+import { useEffect, useState } from 'react'
+import { supabase } from '@/lib/supabase'
+import { ThemedText } from '@/components/themed-text'
+import { ThemedView } from '@/components/themed-view'
+import { useAuthContext } from '@/hooks/use-auth-context'
+
+export default function LoginScreen() {
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const [loading, setLoading] = useState(false)
+  const { isLoggedIn, isLoading } = useAuthContext()
+//   useEffect(() => {
+//     if (!isLoading && isLoggedIn) {
+// 		console.log(isLoading, isLoggedIn)
+//       	router.replace('/home') // target route
+//     }
+//   }, [isLoading, isLoggedIn])
+  
+  const signIn = async () => {
+    setLoading(true)
+
+    const { error } = await supabase.auth.signInWithPassword({
+      email,
+      password,
+    })
+
+    console.log(email, password)
+    setLoading(false)
+
+    if (error) {
+      Alert.alert('Login failed', error.message)
+      return
+    }
+
+    router.replace('/home') // go to home after login
+  }
+
+  return (
+    <>
+      <Stack.Screen options={{ title: 'Login' }} />
+
+      <ThemedView style={styles.container}>
+        <ThemedText type="title">Login</ThemedText>
+
+        <TextInput
+          placeholder="Email"
+          autoCapitalize="none"
+          value={email}
+          onChangeText={setEmail}
+          style={styles.input}
+        />
+
+        <TextInput
+          placeholder="Password"
+          secureTextEntry
+          value={password}
+          onChangeText={setPassword}
+          style={styles.input}
+        />
+
+        <Button
+          title={loading ? 'Logging in...' : 'Login'}
+          onPress={signIn}
+          disabled={loading}
+        />
+      </ThemedView>
+    </>
+  )
+}
+
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    padding: 20,
+  },
+  link: {
+    marginTop: 15,
+    paddingVertical: 15,
+  },
+  input: {
+    width: '100%',
+    height: 48,
+    borderWidth: 1,
+    borderColor: '#ccc',
+    borderRadius: 8,
+    paddingHorizontal: 12,
+    marginVertical: 8,
+    color: 'white',
+  },
+})
