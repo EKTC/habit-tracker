7c447a2927c49a3d1738b87bc32bf2d55481ace8
EKTC <ethancao@y7mail.com>
2026-02-17 22:03:18 +1100

    Changed to use component button

--- app/app/(tabs)/settings/index.tsx
+++ app/app/(tabs)/settings/index.tsx
@@ -9,12 +9,22 @@ import { Link } from "expo-router";
 import React from "react";
 import { Text, View } from "react-native";
 import SignOutButton from "@/components/sign-out-button";
+import Button from "@/components/Button";
+import { supabase } from "@/lib/supabase";
+
+async function onSignOutButtonPress() {
+  const { error } = await supabase.auth.signOut();
+
+  if (error) {
+    console.error("Error signing out:", error);
+  }
+}
 
 export default function HomeScreen() {
   return (
-    <View>
-      <Text style={{ color: "green", fontSize: 20 }}>Help me</Text>
-      <SignOutButton />
+    <View className="flex-1 items-center justify-center bg-background p-4">
+      <Text className="text-xl font-bold text-black">Settings</Text>
+      <Button title="Sign out" onPress={onSignOutButtonPress}></Button>
     </View>
   );
 }
